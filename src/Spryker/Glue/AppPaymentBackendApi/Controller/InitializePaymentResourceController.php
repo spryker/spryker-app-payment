1acba4de26d53d2b1cfefa00c256856e39947db8
vol4onok <alexander.veselov@spryker.com>
2024-10-03 16:30:32 +0300

    ACP-2726: debug

--- src/Spryker/Glue/AppPaymentBackendApi/Controller/InitializePaymentResourceController.php
+++ src/Spryker/Glue/AppPaymentBackendApi/Controller/InitializePaymentResourceController.php
@@ -10,16 +10,21 @@ namespace Spryker\Glue\AppPaymentBackendApi\Controller;
 use Generated\Shared\Transfer\GlueRequestTransfer;
 use Generated\Shared\Transfer\GlueResponseTransfer;
 use Spryker\Glue\Kernel\Backend\Controller\AbstractController;
+use Spryker\Shared\Log\LoggerTrait;
 
 /**
  * @method \Spryker\Glue\AppPaymentBackendApi\AppPaymentBackendApiFactory getFactory()
  */
 class InitializePaymentResourceController extends AbstractController
 {
+    use LoggerTrait;
+
     public function postAction(GlueRequestTransfer $glueRequestTransfer): GlueResponseTransfer
     {
         $initializePaymentRequestTransfer = $this->getFactory()->createGlueRequestPaymentMapper()->mapGlueRequestTransferToInitializePaymentRequestTransfer($glueRequestTransfer);
         $initializePaymentResponseTransfer = $this->getFactory()->getAppPaymentFacade()->initializePayment($initializePaymentRequestTransfer);
+        $this->getFactory()->createGlueResponsePaymentMapper()->mapInitializePaymentResponseTransferToSingleResourceGlueResponseTransfer($initializePaymentResponseTransfer);
+        $this->getLogger()->info('initializePaymentResponse', $initializePaymentResponseTransfer->toArray());
 
         return $this->getFactory()->createGlueResponsePaymentMapper()->mapInitializePaymentResponseTransferToSingleResourceGlueResponseTransfer($initializePaymentResponseTransfer);
     }
